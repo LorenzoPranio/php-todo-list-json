e0a21b5b8ba9c3fb42970cddbebda09f50eb4182
LorenzoPranio <lorenzopranio02@gmail.com>
2024-09-09 15:50:36 +0200

    [UPD]Added script.js

--- index.php
+++ index.php
@@ -7,11 +7,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
     <title>PHP ToDo List JSON</title>
 </head>
 <body>
 
 
-    <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
+    <script src="./script.js"></script>
 </body>
 </html>
