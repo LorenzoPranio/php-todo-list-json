2015a600c92faf5d90f90984f20ead4c01a32392
LorenzoPranio <lorenzopranio02@gmail.com>
2024-09-09 17:49:12 +0200

    [UPD]Finished bonus 2

--- index.php
+++ index.php
@@ -5,15 +5,25 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/1.7.7/axios.min.js" integrity="sha512-DdX/YwF5e41Ok+AI81HI8f5/5UsoxCVT9GKYZRIzpLxb8Twz4ZwPPX+jQMwMhNQ9b5+zDEefc+dcvQoPWGNZ3g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="./style.css">
     <title>PHP ToDo List JSON</title>
 </head>
 <body>
     <div id="app">
-        <ul>
-            <li v-for="(item, index) in todoList">
-                {{ item.name }}
-            </li>
-        </ul>
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <h1 class="text-center my-4 fw-bolder">To Do List</h1>
+                    <ul class="text-center list-unstyled">
+                        <li class="my-2" v-for="(item, index) in todoList" :class="{'task-done' : item.done}">
+                            {{ item.name }}
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        
     </div>
     <script src="./script.js"></script>
 </body>
